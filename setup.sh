88a5183d18c19cdf15178f3195ae3e2e09f78054
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-11 20:22:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -161,7 +161,7 @@ fi
 echo ""
 read -r -p "Do you want to install Firefox? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    sudo pacman -S --needed --noconfirm firefox
+    sudo pacman -S --needed --noconfirm firefox firefox-ublock-origin
 fi
 
 echo ""
