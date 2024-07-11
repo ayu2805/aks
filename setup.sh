5286c2eb1506bd1f6c8237c67305ddbfcb839d1a
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-11 19:57:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -125,6 +125,7 @@ echo ""
 echo "Installing KDE..."
 echo ""
 sudo pacman -S --needed --noconfirm - < kde
+sudo mkdir -p /etc/sddm.conf.d/
 echo -e "[General]\nNumlock=on\nInputMethod=qtvirtualkeyboard\n\n[Theme]\nCurrent=breeze\nCursorTheme=breeze_cursors" | sudo tee /etc/sddm.conf.d/kde_settings.conf > /dev/null
 sudo sed -i 's/^background=.*/background=\/usr\/share\/wallpapers\/Next\/contents\/images_dark\/5120x2880.png/' /usr/share/sddm/themes/breeze/theme.conf
 sudo systemctl enable sddm
