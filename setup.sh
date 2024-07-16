e73602f0edceafc4089c9f68089536f7d084115a
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-17 00:25:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -129,6 +129,7 @@ sudo pacman -S --needed --noconfirm - < kde
 sudo mkdir -p /etc/sddm.conf.d/
 echo -e "[General]\nNumlock=on\nInputMethod=qtvirtualkeyboard\n\n[Theme]\nCurrent=breeze\nCursorTheme=breeze_cursors" | sudo tee /etc/sddm.conf.d/kde_settings.conf > /dev/null
 sudo sed -i 's/^background=.*/background=\/usr\/share\/wallpapers\/Next\/contents\/images_dark\/5120x2880.png/' /usr/share/sddm/themes/breeze/theme.conf
+echo -e "[Icon Theme]\nInherits=breeze_cursors" | sudo tee /usr/share/icons/default/index.theme > /dev/null
 sudo systemctl enable sddm
 
 echo -e "[General]\nRememberOpenedTabs=false" | tee ~/.config/dolphinrc > /dev/null
