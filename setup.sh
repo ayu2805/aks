b5d8ced0239306b752f6248ffb47df3c05b48f66
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-17 22:26:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,10 +51,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
     sudo pacman -Syu
 
-    if [ "$(pactree -r yay-bin)" ] ; then
-        echo ""
-        echo "Yay is already installed"
-    elif [ "$(pactree -r yay)"] ; then
+    if [ "$(pactree -r yay || pactree -r yay-bin)" ]; then
         echo ""
         echo "Yay is already installed"
     else
@@ -62,10 +59,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     fi
 fi
 
-if [ "$(pactree -r yay-bin)" ] ; then
-    echo ""
-    echo "Yay is already installed"
-elif [ "$(pactree -r yay)"] ; then
+if [ "$(pactree -r yay || pactree -r yay-bin)" ]; then
     echo ""
     echo "Yay is already installed"
 else
